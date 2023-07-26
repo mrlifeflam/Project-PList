0d92c68f2488f2cb0334df069ab4f43b17e9aed5
mrlifeflam <73970117+mrlifeflam@users.noreply.github.com>
2023-07-26 13:03:04 -0700

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -47,7 +47,6 @@ local DisplayNames = Instance.new("BoolValue", config)
 DisplayNames.Name = "DisplayNames"
 DisplayNames.Value = false
 game.RunService.RenderStepped:Connect(function()
-    reporttab["In your own words, help us understand what went wrong.Frame"].TextBox.Text = reporttab["In your own words, help us understand what went wrong.Frame"].TextBox.Text:gsub("In your own words, help us understand what went wrong.", "Short Description (Optional)")
     if settingstab.Visible == true then
         menucontainer.PageViewClipper.PageView.CanvasPosition = Vector2.new(0, 0)
         menucontainer.PageViewClipper.PageView.CanvasSize = UDim2.new(0, 0, 0, 0)
